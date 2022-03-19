8c1c684be10c8870091100c73c9adc670cd776ba
shruthi093 <jayasruthi.l@gmail.com>
2022-03-19 15:51:38 +0530

    19.03

--- src/components/TodoComponent.js
+++ src/components/TodoComponent.js
@@ -2,44 +2,16 @@ import React from "react";
 import PendingComponent from "./PendingComponent"
 import CompletedComponent from "./CompletedComponent";
 import BinComponent from "./BinComponent";
-const tododata=[
-    {
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false,
-        "active":true
-      },
-      {
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": true,
-        "active":true
-      },
-      {
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false,
-        "active":true
-      },
-      {
-        "id": 4,
-        "title": "et porro tempora",
-        "completed": true,
-        "active":true
-      },
-      {
-        "id": 5,
-        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        "completed": false,
-        "active":true
-      },
-]
+
 class TodoComponent extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            todos:tododata
+            todos:[]
         }
+    }
+    static getDerivedStateFromProps(props,state){
+      return {todos:props.prop1}
     }
     getCompletedItems=()=>{
       const arr=this.state.todos.filter((item)=>item.completed===true)
